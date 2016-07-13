6056fac424acca79f6c3821e3d38a265ebf1ddd3
Bailey <bailey.g.weathers@gmail.com>
2016-07-12 20:37:42 -0500

    Complete assignment 34

--- spec/controllers/sponsored_posts_controller_spec.rb
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe SponsoredPostsController, type: :controller do
 
     let(:my_topic) { Topic.create!(name:  RandomData.random_sentence, description: RandomData.random_paragraph) }
 
-    let(:my_sponsored_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 25) }
+    let(:my_sponsored_post) { my_topic.sponsored_posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 25) }
 
 
   ##########################
@@ -50,29 +50,31 @@ describe "GET show" do
 
 ####################
 
+describe "GET edit" do
+
+     it "returns http success" do
+       get :edit, topic_id: my_topic.id, id: my_sponsored_post.id
+       expect(response).to have_http_status(:success)
+     end
+
+     it "renders the #edit view" do
+       get :edit, topic_id: my_topic.id, id: my_sponsored_post.id
+       expect(response).to render_template :edit
+     end
+
+
+     it "assigns post to be updated to @sponsored_post" do
+       get :edit, topic_id: my_topic.id, id: my_sponsored_post.id
+
+       sponsored_post_instance = assigns(:sponsored_post)
+
+       expect(sponsored_post_instance.id).to eq my_sponsored_post.id
+       expect(sponsored_post_instance.title).to eq my_sponsored_post.title
+       expect(sponsored_post_instance.body).to eq my_sponsored_post.body
+     end
+   end
+
+#######################
 
 
 end
-
-=begin
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
-=end
