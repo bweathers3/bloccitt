153d3461043562cfeaf849b01c19c2656660d34e
Bailey <bailey.g.weathers@gmail.com>
2016-07-14 08:48:23 -0500

    mid lesson 1

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,9 @@ class Post < ActiveRecord::Base
 
    has_many :comments, dependent: :destroy
 
+
+   validates :title, length: { minimum: 5 }, presence: true
+   validates :body, length: { minimum: 20 }, presence: true
+   validates :topic, presence: true
+
 end
