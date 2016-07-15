4b8a0f434a2723270c85459422d1dcabb7be287e
Bailey <bailey.g.weathers@gmail.com>
2016-07-15 15:48:54 -0500

    complete assignment 35

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,8 @@ class Topic < ActiveRecord::Base
 
      has_many :posts, dependent: :destroy
 
+     validates :name, length: { minimum: 4 }, presence: true
+     validates :description, length: { minimum: 15 }, presence: true
+     validates :public, presence: true
+
 end
