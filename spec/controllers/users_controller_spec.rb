f43203c254fcfa55429b9debbbbc4f3908925d73
Bailey <bailey.g.weathers@gmail.com>
2016-07-17 11:53:50 -0500

    complete checkpoint 38

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -59,4 +59,12 @@ describe "POST create" do
      end
    end
 
+   ############
+
+      it "logs the user in after sign up" do
+        post :create, user: new_user_attributes
+        expect(session[:user_id]).to eq assigns(:user).id
+      end
+   ############
+
 end
