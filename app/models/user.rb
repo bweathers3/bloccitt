adc5233555b825bcb613a39031b245997701092b
Bailey <bailey.g.weathers@gmail.com>
2016-07-19 13:10:05 -0500

    Review for Bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
 
    has_many :posts
-   
-   before_save { self.email = email.downcase if email.present? }
 
+   before_save { self.email = email.downcase if email.present? }
+   before_save { self.role ||= :member }
 
    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
@@ -18,5 +18,6 @@ class User < ActiveRecord::Base
 
    has_secure_password
 
-
+   enum role: [:member, :admin]
+   
 end
