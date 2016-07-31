b04bb78aad3b41bdac889730c667bc2a3d1b22e2
Bailey <bailey.g.weathers@gmail.com>
2016-07-31 03:32:13 -0300

    Complete checkpoint 44

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ActiveRecord::Base
 
    has_many :comments, dependent: :destroy
    has_many :votes, dependent: :destroy
+   has_many :favorites, dependent: :destroy
+   
    has_many :labelings, as: :labelable
    has_many :labels, through: :labelings
 
