31bebbeca3e160d68839243140dc5c811247b9b8
Bailey <bailey.g.weathers@gmail.com>
2016-08-01 13:35:26 -0300

    Update Complete Checkpoint 45 updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,10 +34,11 @@ topics = Topic.all
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
- end
 
-    #post.update_attributes(:created_at, rand(10.minutes .. 1.year).ago)
-    #rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
+    post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+    rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
+
+end
 
     posts = Post.all
 
