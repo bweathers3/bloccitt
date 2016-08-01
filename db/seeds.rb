87c84ba5dedfac38f21872da8c59db288136559e
Bailey <bailey.g.weathers@gmail.com>
2016-08-01 11:05:23 -0300

    holdit

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,14 +34,15 @@ topics = Topic.all
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
+
+    post.update_attributes(:created_at, rand(10.minutes .. 1.year).ago)
+
+    rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
+
  end
 
-posts = Post.all
 
-    #post.update_attributes(:created_at, rand(10.minutes .. 1.year).ago)
-    #rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
-
-
+    posts = Post.all
 
  # Create seed data for Comments; post has many comments
 
