93077efff724e15e3771a47b11ad59f266c01ed7
Bailey <bailey.g.weathers@gmail.com>
2016-08-02 22:25:03 -0300

    stepback

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     def show
      @user = User.find(params[:id])
      @posts = @user.posts.visible_to(current_user)
+     #@favor = @post.favorites
     end
 
 ###########
