6a0361faf55f89e47368b253301ee735d853fc12
Bailey <bailey.g.weathers@gmail.com>
2016-08-04 08:40:07 -0300

    Complete checkpoint-48

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -133,5 +133,13 @@ describe "#favorite_for(post)" do
      end
 
   #############
+  
+     describe "#generate_auth_token" do
+       it "creates a token" do
+         expect(user.auth_token).to_not be_nil
+       end
+     end
+
+  #############
 
 end
