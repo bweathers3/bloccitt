6dad5df552eb8ba31deef730534e462347e19864
Bailey <bailey.g.weathers@gmail.com>
2016-08-04 12:24:04 -0300

    Pause

--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -7,7 +7,7 @@ require 'rails_helper'
   let(:topic) { create(:topic) }
   let(:user) { create(:user) }
   let(:post) { create(:post) }
-  let(:label) { create(:label) }
+  let(:label1) { create(:label) }
   let(:label2) { create(:label) }
 
    it { is_expected.to have_many :labelings }
@@ -16,8 +16,8 @@ require 'rails_helper'
 
    describe "labelings" do
     it "allows the same label to be associated with a different topic and post" do
-      topic.labels << label
-      post.labels << label
+      topic.labels << label1
+      post.labels << label1
 
       topic_label = topic.labels[0]
       post_label = post.labels[0]
@@ -28,8 +28,9 @@ require 'rails_helper'
 
       describe ".update_labels" do
         it "takes a comma delimited string and returns an array of Labels" do
-          labels = "#{label.name}, #{label2.name}"
-          labels_as_a = [label, label2]
+          labels = "#{label1.name}, #{label2.name}"
+          p labels
+          labels_as_a = [label1, label2]
 
           expect(Label.update_labels(labels)).to eq(labels_as_a)
         end
