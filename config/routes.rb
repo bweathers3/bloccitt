65a10d7646e6e515bcd8b5fa7077c80ccfaf47f7
Bailey <bailey.g.weathers@gmail.com>
2016-08-08 00:14:14 -0300

    hold

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,11 @@ Rails.application.routes.draw do
      namespace :v1 do
        resources :users, only: [:index, :show, :create, :update]
        resources :topics, except: [:edit, :new]
+
+       resources :topics do
+          resources :posts, except: [:edit, :new]
+       end
+
      end
    end
 
