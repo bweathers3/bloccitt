4d78e0438c1cb2d9585e2257ac550e559c583b0c
Bailey <bailey.g.weathers@gmail.com>
2016-08-12 10:50:19 -0300

    holdB

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -29,17 +29,17 @@ end
   def update_vote(new_value)
     @post = Post.find(params[:post_id])
     @vote = @post.votes.where(user_id: current_user.id).first
-Rails.logger.info "*******************************"
-Rails.logger.info @vote.inspect
-Rails.logger.info @post.points
+#Rails.logger.info "*******************************"
+#Rails.logger.info @vote.inspect
+#Rails.logger.info @post.points
 
     if @vote
-Rails.logger.info "if @vote = true"
+#Rails.logger.info "if @vote = true"
 
 
       @vote.update_attribute(:value, new_value)
-Rails.logger.info @vote.inspect
-Rails.logger.info @post.points
+#Rails.logger.info @vote.inspect
+#Rails.logger.info @post.points
     else
       @vote = current_user.votes.create(value: new_value, post: @post)
     end
